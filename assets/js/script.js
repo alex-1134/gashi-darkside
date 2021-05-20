731c17d69fc15c6a8164b99348791f67d92e37d7
alex-1134 <alexa.t.lach@gmail.com>
2021-05-20 11:25:14 +0000

    Trial

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const switch1 = document.getElementsByClassName('switch1')[0]
+const menuli = document.getElementById('menu li')[0]
+switch1.addEventListener('click', () => {
+menuli.classList.switch1('active')
+})
